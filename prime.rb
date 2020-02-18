94bf801348fc7c09a0b38b1297b9d437d2ba5569
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-18 20:47:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
-def prime(num)
-  list_of_divs = (0..num).to_a
+def prime?(num)
+
+  list_of_divs = (2..num-1).to_a
+
+  i = 0
+  while i < list_of_divs.length
+    if num % list_of_divs[i] == 0
+      return false
+    else
+      i += 1
+    end
+    return true
+  end
 end
