82eeae72d052aba0ae705bd29670326a07051ba7
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-18 21:16:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime?(num)
+
+  if num < 0
+    num*=-1
+  end
+
+  list = (1..num).to_a
+  new_list = []
+
+  if list.length == 0 || list.length == 1
+    false
+  elsif list.length < 3
+    true
+  else
+    list.each do |div|
+      if num % div == 0
+        new_list << div
+      end
+    end
+    if new_list.length > 2
+      return false
+    else
+      return true
+    end
+  end
+end
